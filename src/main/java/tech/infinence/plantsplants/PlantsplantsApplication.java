cbafa226c9d58cb96dc5dbd73e6a07c540b5179e
Mukul Garg <Mukul Garg@DESKTOP-JN4NMMB>
2019-01-19 22:45:22 +0530

    Initial commit of Spring boot project.

---
+++ src/main/java/tech/infinence/plantsplants/PlantsplantsApplication.java
@@ -0,0 +1,14 @@
+package tech.infinence.plantsplants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantsplantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantsplantsApplication.class, args);
+	}
+
+}
+
